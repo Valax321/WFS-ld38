1ae2c222a427f8c23bf9bce3012fb945a7c902f2
Andrew Castillo <valax321@gmail.com>
2017-04-22 12:16:02 +1000

    Planet Generator
    - TODO: Need to give this guy money!

---
+++ WFS-ld38/Assets/ld38/Scripts/Dev/SceneLoadEventDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[AddComponentMenu("LD38/On Scene Load")]
+public class SceneLoadEventDispatcher : MonoBehaviour
+{
+    public UnityEvent OnLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (OnLoad != null)
+        {
+            OnLoad.Invoke();
+        }
+    }
+}
