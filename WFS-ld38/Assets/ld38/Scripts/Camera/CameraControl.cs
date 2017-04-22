5e46358d9acfa89797b7eadb06b52ae29f957c27
David W <productionsbyamethyst@gmail.com>
2017-04-22 15:29:31 +1000

    Camera Control Script

---
+++ WFS-ld38/Assets/ld38/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    #region Declarations
+    [Range(0, 10)]
+    public float zoomLevel;
+    public float zoomSpeed = 50.0f;
+    public float moveSpeed = 50.0f;
+    public bool zoomEnabled = true;
+    public bool orbitingPlanet = true;
+    public GameObject target = null;
+    #endregion
+
+	void Update ()
+    {
+		if (target != null)
+        {
+            //transform.LookAt(target.transform);
+
+            if (orbitingPlanet)
+            {
+                float moveHorizontal = Input.GetAxis("Horizontal") * -1 * moveSpeed;
+                float moveVertical = Input.GetAxis("Vertical") * moveSpeed;
+                
+                transform.RotateAround(target.transform.position, transform.up, Time.deltaTime * moveHorizontal);
+                transform.RotateAround(target.transform.position, transform.right, Time.deltaTime * moveVertical);
+            }
+
+            //if (zoomEnabled)
+            //{
+            //    float zoom = Input.GetAxis("Zoom") * zoomSpeed;
+            //    transform.Translate(new Vector3(0, Time.deltaTime * zoom, 0), Space.World);
+            //}
+        }
+	}
+}
