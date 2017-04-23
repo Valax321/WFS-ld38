ec66aebe9a584dfa43f9f7c11fff2a72cd6dd190
David W <productionsbyamethyst@gmail.com>
2017-04-23 18:43:04 +1000

    ADDED: Function to spawn Capital

---
+++ WFS-ld38/Assets/ld38/Scripts/TurnBased/SpawnCapital.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Assets;
+
+public class SpawnCapital : MonoBehaviour {
+
+    public Unit unit;
+    public GameObject planet;
+    public GameController scriptGameController;
+
+    private GameObject plate;
+    private VoronoiTile tile;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateCapital()
+    {
+        //
+        // FILE IT UNDER DIFFERENT PLAYER
+        //
+
+        bool foundLandPlate = false;
+        var possibleChild = Enumerable.Range(1, planet.transform.childCount-1).ToList();
+        while (!foundLandPlate)
+        {
+            int num = Random.Range(0, possibleChild.Count);
+            plate = planet.transform.GetChild(possibleChild[Mathf.FloorToInt(num)]).gameObject;
+            TectonicPlate scriptPlate = plate.GetComponent<TectonicPlate>();
+            if (scriptPlate.isLand && plate.transform.childCount > 1)
+            {
+                foundLandPlate = true;
+            }
+            else
+            {
+                possibleChild.RemoveAt(num);
+            }
+        }
+
+        do
+        {
+            tile = plate.transform.GetChild(Mathf.FloorToInt(Random.Range(0, plate.transform.childCount))).gameObject.GetComponent<VoronoiTile>();
+        } while (tile.occupyingUnit != null);
+
+        var go = new GameObject(unit.unitName, typeof(UnitController));
+        Debug.Log(tile.altitude);
+        go.transform.position = tile.centerPoint;
+        var crtl = go.GetComponent<UnitController>();
+        crtl.unitType = unit;
+        crtl.currentTile = tile;
+        crtl.InitUnit();
+        tile.occupyingUnit = crtl;
+    }
+}
