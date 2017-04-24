96d2759b8e7c0bed8630781353a5ec6bec9d0b54
Andrew Castillo <valax321@gmail.com>
2017-04-24 21:30:04 +1000

    Units

---
+++ WFS-ld38/Assets/ld38/Scripts/Environment/WhaleModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleModel : MonoBehaviour
+{
+    public GameObject landModel;
+    public GameObject seaModel;
+
+    void Update()
+    {
+        if (GetComponentInParent<UnitController>() != null)
+        {
+            var unit = GetComponentInParent<UnitController>();
+            if (unit.currentTile != null)
+            {
+                var biome = unit.currentTile.baseBiome;
+                switch (biome)
+                {
+                    case Assets.VoronoiTile.Biomes.Water:
+                        seaModel.SetActive(true);
+                        landModel.SetActive(false);
+                        break;
+                    default:
+                        seaModel.SetActive(false);
+                        landModel.SetActive(true);
+                        break;
+                }
+            }
+        }
+    }
+}
