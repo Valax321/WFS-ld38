36291f999de4531753f580fa947c6abfbb11c696
David W <productionsbyamethyst@gmail.com>
2017-04-25 07:18:16 +1000

    Ability Pool

---
+++ WFS-ld38/Assets/ld38/Scripts/Units/AbilityPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gameplay/AbilityPool")]
+public class AbilityPool : ScriptableObject {
+
+    public List<Ability> abilities;
+}
