c5cae473fe22fd535b01864faaa1deaacae11340
David W <productionsbyamethyst@gmail.com>
2017-04-24 14:53:34 +1000

    Attack Range Boundary Display Fixed

--- WFS-ld38/Assets/ld38/Scripts/TurnBased/BoundaryOutliner.cs
+++ WFS-ld38/Assets/ld38/Scripts/TurnBased/BoundaryOutliner.cs
@@ -10,22 +10,40 @@ public class BoundaryOutliner : MonoBehaviour
 
     void Awake()
     {
-        line = GetComponent<LineRenderer>();        
+        line = GetComponent<LineRenderer>();
     }
 
-    public void MakeBoundary(List<VoronoiTile> tiles)
-    {        
+    public void MakeBoundary(List<VoronoiTile> tilesList)
+    {
+        List<VoronoiTile> tiles = SortList(tilesList);
         if (line != null)
         {
             List<Vector3> positions = new List<Vector3>();
-            positions.AddRange(tiles.Select(x => x.centerPoint));
-            line.numPositions = tiles.Count;
+            positions.AddRange(tiles.Select(x => x.altitude > 0 ? (x.centerPoint + (x.centerPoint - Vector3.zero) * x.altitude) : x.centerPoint));
+            line.numPositions = tiles.Count+1;
             line.SetPositions(positions.ToArray());
+            line.SetPosition(line.numPositions - 1, positions[0]);
         }
+    }
+
+    List<VoronoiTile> SortList(List<VoronoiTile> list)
+    {
+        List<VoronoiTile> tempList = new List<VoronoiTile>() { list[0] };
+        list.RemoveAt(0);
+        int count = list.Count;
+        for (int i = 0; i < count; i++)
+        {
+            tempList.Add(list.Where(x => tempList[i].neighbors.Contains(x)).FirstOrDefault());
+            if (i < count-1)
+            {
+                list.Remove(tempList[i + 1]);
+            }
+        }
+        return tempList;
     }
 
     public void RemoveBoundary()
     {
-        
+        line.numPositions = 0;
     }
 }
